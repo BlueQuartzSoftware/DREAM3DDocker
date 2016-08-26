60046049b95a7a184a6612c140ef45eeeb503d7a
Francois Budin <francois.budin@gmail.com>
2016-08-26 12:15:13 -0400

    ENH: Addition of a script to automatically update SHA1 of DREAM3D repos
    DREAM3D, CMP, SIMPL, and SIMPLView repos SHA1 can be updated automatically
    using the new script UpdateDockerfile.sh. A new git branch is created,
    Dockerfile is updated, and changes are committed.

---
+++ dream3d/UpdateDockerfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+current_date=`date +%Y-%m-%d`
+git checkout -b update_DREAM3D_$current_date
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sha=`git ls-remote https://github.com/BlueQuartzSoftware/DREAM3D.git| grep HEAD | awk '{ print $1}'`
+echo "New DREAM3D SHA: $sha"
+sed -i "s/ENV DREAM3D_VERSION.*$/ENV DREAM3D_VERSION $sha/" Dockerfile
+
+sha=`git ls-remote https://github.com/BlueQuartzSoftware/CMP.git| grep HEAD | awk '{ print $1}'`
+echo "New CMP SHA: $sha"
+sed -i "s/ENV CMP_VERSION.*$/ENV CMP_VERSION $sha/" Dockerfile
+
+sha=`git ls-remote https://github.com/BlueQuartzSoftware/SIMPL.git| grep HEAD | awk '{ print $1}'`
+echo "New SIMPL SHA: $sha"
+sed -i "s/ENV SIMPL_VERSION.*$/ENV SIMPL_VERSION $sha/" Dockerfile
+
+sha=`git ls-remote https://github.com/BlueQuartzSoftware/SIMPLView.git| grep HEAD | awk '{ print $1}'`
+echo "New SIMPLView SHA: $sha"
+sed -i "s/ENV SIMPLView_VERSION.*$/ENV SIMPLView_VERSION $sha/" Dockerfile
+
+git commit Dockerfile -m "ENH: Update DREAM.3D repos to $current_date develop branch."
